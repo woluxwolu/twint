f9c7674635ca935acc9232a17e01eaba337e73c0
minamotorin <76122224+minamotorin@users.noreply.github.com>
2022-02-06 13:59:36 +0000

    Related to twintproject/twint#1335
    This commit doesn't completely fix the problem, but `twint.Profile` works

--- twint/feed.py
+++ twint/feed.py
@@ -59,9 +59,12 @@ def _get_cursor(response):
         next_cursor = response['timeline']['instructions'][0]['addEntries']['entries'][-1]['content'][
             'operation']['cursor']['value']
     except KeyError:
-        # this is needed because after the first request location of cursor is changed
-        next_cursor = response['timeline']['instructions'][-1]['replaceEntry']['entry']['content']['operation'][
-            'cursor']['value']
+        try:
+            # this is needed because after the first request location of cursor is changed
+            next_cursor = response['timeline']['instructions'][-1]['replaceEntry']['entry']['content']['operation'][
+                'cursor']['value']
+        except KeyError:
+            next_cursor = response['timeline']['instructions'][0]['entries'][-1]['content']['value']
     return next_cursor
 
 
@@ -77,46 +80,54 @@ def Json(response):
 def parse_tweets(config, response):
     logme.debug(__name__ + ':parse_tweets')
     response = loads(response)
-    if len(response['globalObjects']['tweets']) == 0:
-        msg = 'No more data!'
-        raise NoMoreTweetsException(msg)
     feed = []
-    for timeline_entry in response['timeline']['instructions'][0]['addEntries']['entries']:
-        # this will handle the cases when the timeline entry is a tweet
-        if (config.TwitterSearch or config.Profile) and (timeline_entry['entryId'].startswith('sq-I-t-') or
-                                                         timeline_entry['entryId'].startswith('tweet-')):
-            if 'tweet' in timeline_entry['content']['item']['content']:
-                _id = timeline_entry['content']['item']['content']['tweet']['id']
-                # skip the ads
-                if 'promotedMetadata' in timeline_entry['content']['item']['content']['tweet']:
-                    continue
-            elif 'tombstone' in timeline_entry['content']['item']['content'] and 'tweet' in \
-                    timeline_entry['content']['item']['content']['tombstone']:
-                _id = timeline_entry['content']['item']['content']['tombstone']['tweet']['id']
-            else:
-                _id = None
-            if _id is None:
-                raise ValueError('Unable to find ID of tweet in timeline.')
-            try:
-                temp_obj = response['globalObjects']['tweets'][_id]
-            except KeyError:
-                logme.info('encountered a deleted tweet with id {}'.format(_id))
+    if 'globalObjects' in response:
+        if len(response['globalObjects']['tweets']) == 0:
+            msg = 'No more data!'
+            raise NoMoreTweetsException(msg)
+        for timeline_entry in response['timeline']['instructions'][0]['addEntries']['entries']:
+            # this will handle the cases when the timeline entry is a tweet
+            if (config.TwitterSearch or config.Profile) and (timeline_entry['entryId'].startswith('sq-I-t-') or
+                                                             timeline_entry['entryId'].startswith('tweet-')):
+                if 'tweet' in timeline_entry['content']['item']['content']:
+                    _id = timeline_entry['content']['item']['content']['tweet']['id']
+                    # skip the ads
+                    if 'promotedMetadata' in timeline_entry['content']['item']['content']['tweet']:
+                        continue
+                elif 'tombstone' in timeline_entry['content']['item']['content'] and 'tweet' in \
+                        timeline_entry['content']['item']['content']['tombstone']:
+                    _id = timeline_entry['content']['item']['content']['tombstone']['tweet']['id']
+                else:
+                    _id = None
+                if _id is None:
+                    raise ValueError('Unable to find ID of tweet in timeline.')
+                try:
+                    temp_obj = response['globalObjects']['tweets'][_id]
+                except KeyError:
+                    logme.info('encountered a deleted tweet with id {}'.format(_id))
 
-                config.deleted.append(_id)
-                continue
-            temp_obj['user_data'] = response['globalObjects']['users'][temp_obj['user_id_str']]
-            if 'retweeted_status_id_str' in temp_obj:
-                rt_id = temp_obj['retweeted_status_id_str']
-                _dt = response['globalObjects']['tweets'][rt_id]['created_at']
-                _dt = datetime.strptime(_dt, '%a %b %d %H:%M:%S %z %Y')
-                _dt = utc_to_local(_dt)
-                _dt = str(_dt.strftime(Tweet_formats['datetime']))
-                temp_obj['retweet_data'] = {
-                    'user_rt_id': response['globalObjects']['tweets'][rt_id]['user_id_str'],
-                    'user_rt': response['globalObjects']['tweets'][rt_id]['full_text'],
-                    'retweet_id': rt_id,
-                    'retweet_date': _dt,
-                }
-            feed.append(temp_obj)
+                    config.deleted.append(_id)
+                    continue
+                temp_obj['user_data'] = response['globalObjects']['users'][temp_obj['user_id_str']]
+                if 'retweeted_status_id_str' in temp_obj:
+                    rt_id = temp_obj['retweeted_status_id_str']
+                    _dt = response['globalObjects']['tweets'][rt_id]['created_at']
+                    _dt = datetime.strptime(_dt, '%a %b %d %H:%M:%S %z %Y')
+                    _dt = utc_to_local(_dt)
+                    _dt = str(_dt.strftime(Tweet_formats['datetime']))
+                    temp_obj['retweet_data'] = {
+                        'user_rt_id': response['globalObjects']['tweets'][rt_id]['user_id_str'],
+                        'user_rt': response['globalObjects']['tweets'][rt_id]['full_text'],
+                        'retweet_id': rt_id,
+                        'retweet_date': _dt,
+                    }
+                feed.append(temp_obj)
+    else:
+        response = response['data']['user']['result']['timeline']
+        for timeline_entry in response['timeline']['instructions'][0]['entries']:
+            if timeline_entry['content'].get('itemContent'):
+                temp_obj = timeline_entry['content']['itemContent']['tweet_results']['result']['legacy']
+                temp_obj['user_data'] = timeline_entry['content']['itemContent']['tweet_results']['result']['core']['user_results']['result']['legacy']
+                feed.append(temp_obj)
     next_cursor = _get_cursor(response)
     return feed, next_cursor
